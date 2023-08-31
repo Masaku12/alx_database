2de0bf750460d068f8c4bdcd9e2809bfabcbbbe5
Ambrose <ambrose.matata11@gmail.com>
2023-08-31 22:23:17 +0300

    Hot fix

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 #!/bin/bash
 -- a script that creates a table
-CREATE TABLE IF NOT EXISTS first_table
+CREATE TABLE IF NOT EXISTS first_table 
 (
-id int,
-name varchar(256),
+id INT, 
+name VARCHAR(256),
 );

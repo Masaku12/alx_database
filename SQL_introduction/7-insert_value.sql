53f8244405d100c23ba0e859d3765a74d1a25e38
Ambrose <ambrose.matata11@gmail.com>
2023-08-31 22:53:03 +0300

    Updated script that inserts a new row

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
 #!/bin/bash
 -- a script that inserts a new row in the existing table
-INSERT INTO first_table(id, name)
-VALUES (89, Holberton School);
+INSERT INTO first_table (id, name) VALUES ('89', 'Holberton School');

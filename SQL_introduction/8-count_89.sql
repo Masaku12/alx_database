ad06d505bfedbc1dd4472f85b6ea8d1c3fc19810
Ambrose <ambrose.matata11@gmail.com>
2023-08-31 23:00:33 +0300

    hot fix

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,5 +1,3 @@
 #!/bin/bash
 -- a script that displays the number of records with id = 89 in a table
-SELECT COUNT (*) 
-FROM first_table 
-WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;

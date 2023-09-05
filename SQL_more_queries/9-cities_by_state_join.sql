3ff4d8dd758d1b3ff209921d443c009cb63e9df6
Ambrose <ambrose.matata11@gmail.com>
2023-09-05 14:11:04 +0300

    hot fix

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -5,5 +5,5 @@
 -- List all cities with their ID, name, and name of the states they are in
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
-JOIN states ON cities.states_id = states.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;

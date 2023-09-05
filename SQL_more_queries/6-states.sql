7a7d937b4fb57138fe879a913db5dad9cb1fd639
Ambrose <ambrose.matata11@gmail.com>
2023-09-05 13:08:37 +0300

    hot fix

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -11,6 +11,5 @@ USE DATABASE hbtn_0d_usa;
 -- Create the table if it does not exist
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    UNIQUE KEY (id),
     name VARCHAR(256) NOT NULL
 );

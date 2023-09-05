19424dadb3394727992c1eb50a05028682553142
Ambrose <ambrose.matata11@gmail.com>
2023-09-05 12:51:56 +0300

    hot fix

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -3,6 +3,6 @@
 -- This script creates a table unique_id on my MySQL server
 CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1,
-    UNIQUE id,
-    name VARCHAR(256)
-)
+    name VARCHAR(256),
+    UNIQUE KEY (id)
+);

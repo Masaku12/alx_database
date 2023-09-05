9f6f2595ecddbf0cabcb393039bfbfcd25a89d33
Ambrose <ambrose.matata11@gmail.com>
2023-09-05 12:10:32 +0300

    updated script that creates a MySQL user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 -- This script creates the MySQL server user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1pd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;

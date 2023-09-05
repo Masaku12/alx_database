6125ffd7c3a93af0b5aac9b50b6ca5eb2cb5b8e6
Ambrose <ambrose.matata11@gmail.com>
2023-09-05 13:11:02 +0300

    hot fix

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 -- This script creates a database hbtn_0d_usa and table states
-
--- Create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Use the specified database
-USE DATABASE hbtn_0d_usa;
-
--- Create the table if it does not exist
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL

d20caab080269813ee7cf66b4a0eba24665fc9ca
Ambrose <ambrose.matata11@gmail.com>
2023-09-05 14:09:05 +0300

    updated script that lists all cities

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 -- This script lists all cities in the database hbtn_0d_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
 
 -- List all cities with their ID, name, and name of the states they are in
 SELECT cities.id, cities.name, states.name AS state_name
